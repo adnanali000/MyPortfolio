a2fd3e472776c950c825192facb20703968f88ea
Adnan musil <adnan.museel123@gmail.com>
2020-11-13 15:56:53 +0500

    myPortfolio narbar deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import './App.css';
 import React from 'react';
-
+import {BrowserRouter as Router,Route,Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Adnan',
+      headerLinks: [
+        {title:'Home',path:'/'},
+        {title:'About',path:'/about'},
+        {title:'Contact',path:'/contact'},
+      ],
+      home: {
+        title: 'Be Rentless',
+        subTitle: 'Projects that make a difference',
+        text: 'Check my project below'
+      },
+      about: {
+        title: 'About me'
+      },
+      contact: {
+        title: 'Let\'s talk'
+      }
+    }
+
+  }
   render(){
     return(
-      <div>
-        <h1>Hello World</h1>
-      </div>
+     <Router>
+       <Container className="p-0" fluid={true}>
+         <Navbar className="border-bottom" bg="transparent" expand="lg">
+           <Navbar.Brand>Adnan Projects</Navbar.Brand>
+           <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>      
+      </Container>
+
+
+     </Router>
     )
   }
 }
