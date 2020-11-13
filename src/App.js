3d83224fc75998585e156a84064316f656caff7f
Adnan musil <adnan.museel123@gmail.com>
2020-11-13 17:03:36 +0500

    myPortfolio link route set

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import {BrowserRouter as Router,Route,Link} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
 
 class App extends React.Component{
   constructor(props){
@@ -41,13 +46,16 @@ class App extends React.Component{
               <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>                
               </Nav>
             </Navbar.Collapse>
           </Navbar> 
 
+          <Route path='/' exact render ={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text = {this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+
           <Footer/>  
-      
       </Container>
      </Router>
     )
